95043500ee1293b8bd1b176fbedcadcef0cdaa04
Rini Yudesia Naswir <azrinie8139@yahoo.com>
2015-09-13 10:17:10 +0800

    Plot 1 R Code File

---
+++ plot1.R
@@ -0,0 +1,18 @@
+household_power <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+names(household_power)
+lapply(household_power, class)
+
+household_power$DateTime <- paste(household_power$Date, household_power$Time)
+household_power$DateTime <- strptime(household_power$DateTime, "%d/%m/%Y %H:%M:%S")
+
+start <- which(household_power$DateTime == strptime("2007-02-01", "%Y-%m-%d"))
+end <- which(household_power$DateTime == strptime("2007-02-02 23:59:00", "%Y-%m-%d %H:%M:%S"))
+household_power <- household_power[start:end,]
+
+hist(as.numeric(as.character(household_power$Global_active_power)),
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col = "red")
+
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
